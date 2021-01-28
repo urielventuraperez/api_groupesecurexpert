a684008c0d33cab084e73128672c4370ccfb34d1
Uriel <venturaperez.10101991@gmail.com>
2021-01-28 07:31:03 -0600

    Feature: Created migrations files

---
+++ database/migrations/2021_01_28_133003_create_range_years_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRangeYearsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('range_years', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('range_years');
+    }
+}
