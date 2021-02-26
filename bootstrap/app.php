082791aec58d9d27d21f35c1b59ee17a746c9d23
Uriel <venturaperez.10101991@gmail.com>
2021-02-26 07:10:14 -0600

    feature: CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,10 @@ $app = new \Dusterio\LumenPassport\Lumen7Application(
     dirname(__DIR__)
 );
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+ ]);
+
 $app->withFacades();
 
 $app->withEloquent();
